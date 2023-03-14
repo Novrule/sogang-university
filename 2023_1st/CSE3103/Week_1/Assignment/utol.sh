0e154b4cca1e4e08f849b1ac1021fd4b8c2ff536
NaranggeSaida <119842443+NaranggeSaida@users.noreply.github.com>
2023-03-14 19:55:40 +0900

    Add files via upload

---
+++ 2023_1st/CSE3103/Week_1/Assignment/utol.sh
@@ -0,0 +1,23 @@
+echo "working directory:"
+read directory
+
+if [ -z "$directory" ]
+then
+	cd
+else     
+	cd $directory  
+	if [ $? -ne 0 ]  
+	then
+		echo "Error"
+		exit 0
+	fi
+fi
+
+for i in *
+do
+	if [ -f $i ]
+	then
+	mv $i `echo $i | tr '[A-Z][a-z]' '[a-z][A-Z]'`
+	fi
+done
+exit 0
