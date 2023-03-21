4e393d8bef406db49b81ccbe5f5e3316d7f990f0
NaranggeSaida <119842443+NaranggeSaida@users.noreply.github.com>
2023-03-21 16:54:16 +0900

    main함수에서 사용될 매크로, 함수를 모아놓은 헤더 파일

--- 2023_1st/CSE3103/Week_2/Lab/Lab_3/Header.h
+++ 2023_1st/CSE3103/Week_2/Lab/Lab_3/Header.h
@@ -4,7 +4,7 @@
 
 #define BNUM 256
 #define LIMIT 72
-ㅁ
+
 void Remove_Blanks_At_The_End(char* line);
 void Print_Line(char* line, int* Count, int* B_Flag);
 void Get_Blanks_Chars(char* line, int Start, int* N_Blanks, int* N_Chars);
