9b3c09f8c152aa86358570af7c1a22fe3cb670a7
NaranggeSaida <119842443+NaranggeSaida@users.noreply.github.com>
2023-04-04 21:02:48 +0900

    Stack의 멤버함수를 모아놓은 헤더 파일

---
+++ 2023_1st/CSE3103/Week_4/Lab/Stack.h
@@ -0,0 +1,28 @@
+#ifndef __STACK__
+#define __STACK__
+
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+template <class T> class Stack : public LinkedList<T> {
+public:
+	bool Delete(T& element);
+};
+
+template <class T> bool Stack<T>::Delete(T& element) {
+    if (LinkedList<T>::current_size == 0) {
+        return false;
+    }
+
+    Node<T>* temp = this->first;
+    element = temp->data;
+    this->first = this->first->link;
+    delete temp;
+    this->current_size--;
+
+    return true;
+}
+
+#endif
