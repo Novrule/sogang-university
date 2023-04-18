920b762269203cb18331a714dc1ac35a3bc3df5a
NaranggeSaida <119842443+NaranggeSaida@users.noreply.github.com>
2023-04-18 16:23:32 +0900

    Array의 멤버함수를 모아놓은 헤더 파일

---
+++ 2023_1st/CSE3013/Week_6/Lab/ofApp.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "ofMain.h"
+
+class ofApp : public ofBaseApp {
+
+public:
+	void setup();
+	void update();
+	void draw();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y);
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);
+
+	int draw_flag;
+	int load_flag;
+
+	int line_num;
+	int dot_num;
+	float diameter;
+
+	typedef struct water_line {
+		int from_x, to_x;
+		int from_y, to_y;
+	} water_line;
+
+	typedef struct water_dot {
+		int x;
+		int y;
+	} water_dot;
+
+	water_line* line_arr;
+	water_dot* dot_arr;
+
+	int index;
+
+	void processOpenFileSelection(ofFileDialogResult openFileResult);
+	void initializeWaterLines();
+};
