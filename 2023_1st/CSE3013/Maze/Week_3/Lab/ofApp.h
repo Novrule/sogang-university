a654849b13f234643d612585dec31fb6d2c4309b
NaranggeSaida <119842443+NaranggeSaida@users.noreply.github.com>
2023-06-07 01:32:27 +0900

    미로를 그려주는 프로그램의 멤버함수를 모아놓은 헤더 파일

--- 2023_1st/CSE3013/Maze/Week_3/Lab/ofApp.h
+++ 2023_1st/CSE3013/Maze/Week_3/Lab/ofApp.h
@@ -47,15 +47,15 @@ public:
 	void DFS();
 	void dfsdraw();
 
-	int HEIGHT; //¹Ì·ÎÀÇ ³ôÀÌ
-	int WIDTH; //¹Ì·ÎÀÇ ³Êºñ
-	char** input; //ÅØ½ºÆ® ÆÄÀÏÀÇ ¸ðµç Á¤º¸¸¦ ´ã´Â ÀÌÂ÷¿ø ¹è¿­ÀÌ´Ù.
-	int** visited; //¹æ¹®¿©ºÎ¸¦ ÀúÀåÇÒ Æ÷ÀÎÅÍ
-	int maze_col; //¹Ì·ÎÄ­ÀÇ ¿­ÀÇ ÀÎµ¦½º¸¦ °¡¸®Å²´Ù.
-	int maze_row; //¹Ì·ÎÄ­ÀÇ ÇàÀÇ ÀÎµ¦½º¸¦ °¡¸®Å²´Ù.
-	int isOpen; //ÆÄÀÏÀÌ ¿­·È´ÂÁö¸¦ ÆÇ´ÜÇÏ´Â º¯¼ö. 0ÀÌ¸é ¾È¿­·È°í 1ÀÌ¸é ¿­·È´Ù.
-	int dx[4] = { 1, -1, 0 ,0 }; //¹Ì·Î¿¡¼­ xÃàÀ¸·Î °¥ ¼ö ÀÖ´Â ¹æÇâ
-	int dy[4] = { 0, 0, 1, -1 }; //¹Ì·Î¿¡¼­ yÃàÀ¸·Î °¥ ¼ö ÀÖ´Â ¹æÇâ
+	int HEIGHT; //ë¯¸ë¡œì˜ ë†’ì´
+	int WIDTH; //ë¯¸ë¡œì˜ ë„ˆë¹„
+	char** input; //í…ìŠ¤íŠ¸ íŒŒì¼ì˜ ëª¨ë“  ì •ë³´ë¥¼ ë‹´ëŠ” ì´ì°¨ì› ë°°ì—´ì´ë‹¤.
+	int** visited; //ë°©ë¬¸ì—¬ë¶€ë¥¼ ì €ìž¥í•  í¬ì¸í„°
+	int maze_col; //ë¯¸ë¡œì¹¸ì˜ ì—´ì˜ ì¸ë±ìŠ¤ë¥¼ ê°€ë¦¬í‚¨ë‹¤.
+	int maze_row; //ë¯¸ë¡œì¹¸ì˜ í–‰ì˜ ì¸ë±ìŠ¤ë¥¼ ê°€ë¦¬í‚¨ë‹¤.
+	int isOpen; //íŒŒì¼ì´ ì—´ë ¸ëŠ”ì§€ë¥¼ íŒë‹¨í•˜ëŠ” ë³€ìˆ˜. 0ì´ë©´ ì•ˆì—´ë ¸ê³  1ì´ë©´ ì—´ë ¸ë‹¤.
+	int dx[4] = { 1, -1, 0 ,0 }; //ë¯¸ë¡œì—ì„œ xì¶•ìœ¼ë¡œ ê°ˆ ìˆ˜ ìžˆëŠ” ë°©í–¥
+	int dy[4] = { 0, 0, 1, -1 }; //ë¯¸ë¡œì—ì„œ yì¶•ìœ¼ë¡œ ê°ˆ ìˆ˜ ìžˆëŠ” ë°©í–¥
 	
 	//Menu
 	ofxWinMenu* menu; //Menu object
