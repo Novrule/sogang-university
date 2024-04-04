eae50a686b9507da174b7be8db4f80d98addfd6f
Novrule <2000dnjswns@naver.com>
2024-04-05 00:52:30 +0900

    update : update CSE_4050 Project_02

--- 2024_1st/CSE_4050/Project_02/CMinus.fs
+++ 2024_1st/CSE_4050/Project_02/CMinus.fs
@@ -11,31 +11,43 @@ let rec evalExp (exp: Exp) (mem: Mem) : Val =
   | False -> Bool false
   | Var x -> 
     match Map.tryFind x mem with
-    | Some temp -> temp
+    | Some v -> v
     | None -> raise UndefinedSemantics
   | Add (e1, e2) -> 
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Int (n1 + n2)
     | _ -> raise UndefinedSemantics
   | Sub (e1, e2) -> 
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Int (n1 - n2)
     | _ -> raise UndefinedSemantics
   | LessThan (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 < n2)
     | _ -> raise UndefinedSemantics
   | GreaterThan (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 > n2)
     | _ -> raise UndefinedSemantics
   | Equal (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
-    | Int n1, Int n2 -> Bool (n1 = n2)
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
+    | Int n1, Int n2 -> Bool (n1 <> n2)
     | Bool b1, Bool b2 -> Bool (b1 = b2)
     | _ -> raise UndefinedSemantics
   | NotEq (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 <> n2)
     | Bool b1, Bool b2 -> Bool (b1 <> b2)
     | _ -> raise UndefinedSemantics
@@ -45,20 +57,20 @@ let rec exec (stmt: Stmt) (mem: Mem) : Mem =
   match stmt with
   | NOP -> mem // NOP does not change the memory.
   | Assign (x, e) -> 
-    let temp = evalExp e mem
-    Map.add x temp mem
+    let v = evalExp e mem
+    Map.add x v mem
   | Seq (s1, s2) ->
     let nextMem = exec s1 mem
     exec s2 nextMem
-  | If (condition, thenStmt, elseStmt) ->
-    match evalExp condition mem with
-    | Bool true -> exec thenStmt mem
-    | Bool false -> exec elseStmt mem
+  | If (e, s1, s2) ->
+    match evalExp e mem with
+    | Bool true -> exec s1 mem
+    | Bool false -> exec s2 mem
     | _ -> raise UndefinedSemantics
-  | While (condition, body) ->
-    match evalExp condition mem with
+  | While (e, s) ->
+    match evalExp e mem with
     | Bool true ->
-      let nextMem = exec body mem
+      let nextMem = exec s mem
       exec stmt nextMem
     | Bool false -> mem
     | _ -> raise UndefinedSemantics
--- 2024_1st/CSE_4050/Project_02/CMinusPtr.fs
+++ 2024_1st/CSE_4050/Project_02/CMinusPtr.fs
@@ -5,86 +5,95 @@ open Types
 
 // Evaluate expression into a value, under the given memory.
 let rec evalExp (exp: Exp) (mem: Mem) : Val =
-    match exp with
+  match exp with
   | Num i -> Int i
   | True -> Bool true
   | False -> Bool false
-  | LV lv -> evalLVal lv mem
+  | LV (Var s) -> 
+    match Map.tryFind s mem with
+    | Some v -> v
+    | None -> raise UndefinedSemantics
+  | LV (Deref e) ->
+    match evalExp e mem with
+    | Loc varName -> 
+      match Map.tryFind varName mem with
+      | Some v -> v
+      | None -> raise UndefinedSemantics
+    | _ -> raise UndefinedSemantics
   | AddrOf varName -> 
     match Map.tryFind varName mem with
-    | Some _ -> Ptr varName
-    | None -> raise UndefinedVariable varName
+    | Some _ -> Loc varName
+    | None -> raise UndefinedSemantics
   | Add (e1, e2) -> 
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Int (n1 + n2)
     | _ -> raise UndefinedSemantics
   | Sub (e1, e2) -> 
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Int (n1 - n2)
     | _ -> raise UndefinedSemantics
   | LessThan (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 < n2)
     | _ -> raise UndefinedSemantics
   | GreaterThan (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 > n2)
     | _ -> raise UndefinedSemantics
   | Equal (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
-    | Int n1, Int n2 -> Bool (n1 = n2)
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
+    | Int n1, Int n2 -> Bool (n1 <> n2)
     | Bool b1, Bool b2 -> Bool (b1 = b2)
     | _ -> raise UndefinedSemantics
   | NotEq (e1, e2) ->
-    match evalExp e1 mem, evalExp e2 mem with
+    let v1 = evalExp e1 mem
+    let v2 = evalExp e2 mem
+    match v1, v2 with
     | Int n1, Int n2 -> Bool (n1 <> n2)
     | Bool b1, Bool b2 -> Bool (b1 <> b2)
     | _ -> raise UndefinedSemantics
-
-and evalLVal (lval: LVal) (mem: Mem) : Val =
-  match lval with
-  | Var varName -> 
-    match Map.tryFind varName mem with
-    | Some v -> v
-    | None -> raise UndefinedVariable varName
-  | Deref exp -> 
-    let valExp = evalExp exp mem
-    match valExp with
-    | Ptr varName -> 
-      match Map.tryFind varName mem with
-      | Some v -> v
-      | None -> raise UndefinedVariable varName
-      | _ -> raise InvalidDereference
 
 // Execute a statement and return the updated memory.
 let rec exec (stmt: Stmt) (mem: Mem) : Mem =
   match stmt with
   | NOP -> mem // NOP does not change the memory.
-  | Assign (lv, exp) -> 
-    let newVal = evalExp exp mem
+  | Assign (lv, e1) ->
     match lv with
-    | Var varName -> Map.add varName newVal mem
-    | Deref exp -> 
-      let valExp = evalExp exp mem
-      match valExp with
-      | Ptr varName -> Map.add varName newVal mem
-      | _ -> raise InvalidAssignment
+    | Var s -> 
+      let v = evalExp e1 mem
+      Map.add s v mem
+    | Deref e2 -> 
+      match evalExp e2 mem with
+      | Loc varName -> 
+        let v = evalExp e1 mem
+        Map.add varName v mem
+      | _ -> raise UndefinedSemantics
   | Seq (s1, s2) ->
     let nextMem = exec s1 mem
     exec s2 nextMem
-  | If (condition, thenStmt, elseStmt) ->
-    match evalExp condition mem with
-    | Bool true -> exec thenStmt mem
-    | Bool false -> exec elseStmt mem
+  | If (e, s1, s2) ->
+    match evalExp e mem with
+    | Bool true -> exec s1 mem
+    | Bool false -> exec s2 mem
     | _ -> raise UndefinedSemantics
-  | While (condition, body) ->
-    match evalExp condition mem with
+  | While (e, s) ->
+    match evalExp e mem with
     | Bool true ->
-      let nextMem = exec body mem
+      let nextMem = exec s mem
       exec stmt nextMem
     | Bool false -> mem
     | _ -> raise UndefinedSemantics
 
 // The program starts execution with an empty memory. Do NOT fix this function.
 let run (prog: Program) : Mem =
-  exec prog Map.empty
+    exec prog Map.empty
